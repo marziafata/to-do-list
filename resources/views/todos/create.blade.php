9fff79034bb1731dabc6207ada734eb31fcdeece
Marzia Fatarella <marziafatarella@gmail.com>
2020-08-19 10:10:07 +0200

    foreach all todos + @yield

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -1,22 +1,15 @@
 {{-- create new to do --}}
+@extends('todos.layout')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <title>Todos</title>
-</head>
-<body>
-    <div class="text-center pt-10">
-        <h1 class="text-2x1">La tua prossima cosa da fare</h1>
-        <x-alert/>
-        <form method="post" action="/todos/create" class="py-5">
-            @csrf
-            <input type="text" name="title" class="p-2 border rounded">
-            <input type="submit" value="Crea" class="p-2 border rounded">
-        </form>
-    </div>
-</body>
-</html>
+@section('content')
+    <h1 class="text-2x1">La tua prossima cosa da fare</h1>
+    <x-alert/>
+    <form method="post" action="/todos/create" class="py-5">
+        @csrf
+        <input type="text" name="title" class="p-2 border rounded">
+        <input type="submit" value="Crea" class="p-2 border rounded">
+    </form>
+@endsection
+
+
+
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,1 +1,15 @@
-all todos
+{{-- all todos  --}}
+
+@extends('todos.layout')
+@section('content')
+<h1 class="text-2x1">Tutte le cose da fare</h1>
+    @foreach ($todos as $todo)
+        <ul>
+            <li>
+                {{$todo->title}}
+            </li>
+        </ul>
+    @endforeach
+@endsection
+
+
